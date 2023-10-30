baf6454276766e2d7fd90e9c4fd607f5d8ae1bbd
Diego581 <diego445.gonzalez@gmail.com>
2023-10-30 19:50:51 -0300

    no funciona

--- src/app/components/JsonViewer.jsx
+++ src/app/components/JsonViewer.jsx
@@ -1,31 +1,34 @@
 'use client'
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const JsonViewer = () => {
-  const [formData, setFormData] = useState({
-    userId: localStorage.getItem(id),
-    // Agrega aquí más campos según tus necesidades
-  });
-  const [jsonData, setJsonData] = useState(null);
+const ApiData = () => {
+  const [data, setData] = useState('');
 
   useEffect(() => {
-    // Cargar el archivo JSON
-    fetch('http://localhost:3000/api/users/models')
-      .then(response => response.json())
-      .then(data => setJsonData(data))
-      .catch(error => console.error('Error al cargar el archivo JSON:', error));
-  }, []);
+    // Obtener el ID almacenado en el localStorage
+    const userId = localStorage.getItem('id');
 
-  if (!jsonData) {
-    return <div>Cargando...</div>;
-  }
+    if (userId) {
+      // Realizar la consulta a la API con Axios
+      axios.get(`http://localhost:3000/api/users/models`, userId)
+        .then((response) => {
+          // Obtener los datos de la respuesta
+          const responseData = response.data;
+          setData(responseData);
+        })
+        .catch((error) => {
+          console.error('Error al realizar la solicitud a la API', error);
+        });
+    }
+  }, []);
 
   return (
     <div>
-      <h2>Contenido del JSON</h2>
-      <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+      <h1>Datos de la API para el ID almacenado en el localStorage:</h1>
+      <p>{data}</p>
     </div>
   );
 };
 
-export default JsonViewer;
+export default ApiData;
